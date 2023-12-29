b95c3f299263c8cb31222c33c761ca176628fdfe
PriyaGupta1996 <priyagupta.ynr@gmail.com>
2023-12-28 21:39:51 -0500

    optimize load csv seeder program

--- seeders/seedDatabase.js
+++ seeders/seedDatabase.js
@@ -1,135 +1,146 @@
-const fs = require('fs')
-const sequelize = require("../database")
-const csv = require("csv-parser")
-const { AgricultureData, Crop, District, Season, State } = require("../models")
-const csvFilePath = "./seeders/India Agriculture Crop Production.csv"
+const fs = require('fs');
+const sequelize = require("../database");
+const csv = require("csv-parser");
+const { AgricultureData, Crop, District, Season, State } = require("../models");
 
-const AVG_COCONUT_WEIGHT = 1.4
-const balesToTonMultiplier = 0.24
+const AVG_COCONUT_WEIGHT = 1.4;
+const balesToTonMultiplier = 0.24;
+const csvFilePath = "./seeders/India Agriculture Crop Production.csv";
+const BULK_INSERT_THRESHOLD = 10000;
 
-const populateCrop = async (cropName) => {
+async function populateEntity(model, field, value) {
     try {
-        let record = await Crop.findOne({ where: { CropName: cropName } });
+        let record = await model.findOne({ where: { [field]: value } });
         if (!record) {
-            record = await Crop.create({ CropName: cropName });
+            record = await model.create({ [field]: value });
         }
-        return record.CropID
-
+        return record.get(`${model.name}ID`);
     } catch (error) {
-        console.log("Error in populateCrop: " + error)
-        throw new Error("Error in populateCrop: " + error);
+        console.log(`Error in populate${model.name}: ${error}`);
+        throw new Error(`Error in populate${model.name}: ${error}`);
     }
-
 }
 
-const populateSeason = async (seasonName) => {
+async function populateCrop(cropName) {
+    return populateEntity(Crop, 'CropName', cropName);
+}
+
+async function populateSeason(seasonName) {
+    return populateEntity(Season, 'SeasonName', seasonName);
+}
+
+async function populateDistrict(districtName) {
+    return populateEntity(District, 'DistrictName', districtName);
+}
+
+async function populateState(stateName) {
+    return populateEntity(State, 'StateName', stateName);
+}
+
+async function processRow(row, bulkInsertData) {
     try {
-        let record = await Season.findOne({ where: { SeasonName: seasonName } });
-        if (!record) {
-            record = await Season.create({ SeasonName: seasonName });
+        const rowData = {
+            state: row['State'].toLowerCase(),
+            district: row['District'].toLowerCase(),
+            season: row['Season'].toLowerCase(),
+            year: row['Year'].split('-')[0],
+            crop: row['Crop'].toLowerCase(),
+            area: row['Area'],
+            areaUnit: row['Area Units'].toLowerCase(),
+            production: row['Production'],
+            cropYield: row['Yield'],
+            productionUnit: row['Production Units'].toLowerCase(),
+        };
+        const [cropID, districtID, seasonID, stateID] = await Promise.all([
+            populateCrop(rowData["crop"]),
+            populateDistrict(rowData["district"]),
+            populateSeason(rowData["season"]),
+            populateState(rowData["state"]),
+        ]);
+
+        bulkInsertData.push(createAgricultureDataObject(cropID, districtID, seasonID, stateID, rowData));
+
+        // Insert in bulk after a certain number of records (e.g., 1000 records)
+        if (bulkInsertData.length === BULK_INSERT_THRESHOLD) {
+            await bulkInsertAgricultureData([...bulkInsertData]);
+            bulkInsertData.length = 0;
         }
-        return record.SeasonID
     } catch (error) {
-        console.log("Error in populateSeason: " + error)
-        throw new Error("Error in populateSeason: " + error);
+        console.log('Error in processing row', error);
     }
 }
 
-const populateDistrict = async (districtName) => {
-    try {
-        let record = await District.findOne({ where: { DistrictName: districtName } });
-        if (!record) {
-            record = await District.create({ DistrictName: districtName });
-        }
-        return record.DistrictID
-    } catch (error) {
-        console.log("Error in populateDistrict: " + error)
-        throw new Error("Error in populateDistrict: " + error);
-    }
+function createAgricultureDataObject(cropID, districtID, seasonID, stateID, rowData) {
+    return {
+        CropID: cropID,
+        DistrictID: districtID,
+        SeasonID: seasonID,
+        StateID: stateID,
+        Year: parseInt(rowData.year),
+        Area: rowData.area,
+        AreaUnit: rowData.areaUnit,
+        Production: rowData.production,
+        ProductionUnit: rowData.productionUnit,
+        Yield: rowData.cropYield,
+    };
 }
 
-const populateState = async (stateName) => {
+async function bulkInsertAgricultureData(data) {
     try {
-        let record = await State.findOne({ where: { StateName: stateName } });
-        if (!record)
-            record = await State.create({ StateName: stateName });
-        return record.get("StateID")
-    } catch (error) {
-        console.log("Error in populateState: " + error)
-        throw new Error("Error in populateState: " + error);
-    }
-}
+        const processedData = data.map((rowData) => {
+            let { year, area, areaUnit, production, cropYield, productionUnit } = rowData;
 
-const populateAgricultureData = async (cropID, districtID, seasonID, stateID, rowData) => {
-    let { year, area, areaUnit, production, cropYield, productionUnit } = rowData
-    if (productionUnit === "nuts") {
-        production = production * (AVG_COCONUT_WEIGHT) / 1000 //converting kgs to tonnes
-        cropYield = production / area
-    }
-    if (productionUnit === "bales") {
-        production = production * (balesToTonMultiplier);
-        cropYield = production / area;
-    }
-    try {
-        let record = await AgricultureData.findOne({
-            where: {
-                CropID: cropID,
-                DistrictID: districtID,
-                SeasonID: seasonID,
-                StateID: stateID,
-                Year: parseInt(year),
+            if (productionUnit === "nuts") {
+                production = production * (AVG_COCONUT_WEIGHT) / 1000; // converting kgs to tonnes
+                cropYield = production / area;
             }
+
+            if (productionUnit === "bales") {
+                production = production * (balesToTonMultiplier);
+                cropYield = production / area;
+            }
+
+            return {
+                CropID: rowData.CropID,
+                DistrictID: rowData.DistrictID,
+                SeasonID: rowData.SeasonID,
+                StateID: rowData.StateID,
+                Year: rowData.Year,
+                Area: rowData.Area,
+                AreaUnit: rowData.AreaUnit,
+                Production: rowData.Production,
+                ProductionUnit: rowData.ProductionUnit,
+                Yield: rowData.Yield,
+            };
         });
-        if (!record) {
-            record = await AgricultureData.create({
-                CropID: cropID,
-                DistrictID: districtID,
-                SeasonID: seasonID,
-                StateID: stateID,
-                Year: parseInt(year),
-                Area: area,
-                AreaUnit: areaUnit,
-                Production: production,
-                ProductionUnit: productionUnit,
-                Yield: cropYield,
-            })
-        }
+
+        await AgricultureData.bulkCreate(processedData, { ignoreDuplicates: true });
     } catch (error) {
-        console.log("Error in populateAgricultureData", error)
-        throw new Error("Error in populateAgricultureData", error)
+        console.log('Error in bulkInsertAgricultureData', error);
+        throw new Error('Error in bulkInsertAgricultureData', error);
     }
 }
-
-const seedDatabase = async () => {
+async function seedDatabase() {
     try {
-        const stream = fs.createReadStream(csvFilePath).pipe(csv());
-        let rowNo = 1;
-        for await (const row of stream) {
-            console.log("row:", rowNo++)
-            //fetching the row data
-            const rowData = {
-                state: row['State'].toLowerCase(),
-                district: row['District'].toLowerCase(),
-                season: row['Season'].toLowerCase(),
-                year: row['Year'].split('-')[0],
-                crop: row['Crop'].toLowerCase(),
-                area: row['Area'],
-                areaUnit: row['Area Units'].toLowerCase(),
-                production: row['Production'],
-                cropYield: row['Yield'],
-                productionUnit: row['Production Units'].toLowerCase()
-            }
-            const [cropID, districtID, seasonID, stateID] = await Promise.all([populateCrop(rowData["crop"]), populateDistrict(rowData["district"]), populateSeason(rowData["season"]), populateState(rowData["state"])])
-            await populateAgricultureData(cropID, districtID, seasonID, stateID, rowData);
+        await sequelize.sync();
 
+        const stream = fs.createReadStream(csvFilePath).pipe(csv());
+        const bulkInsertData = [];
+
+        for await (const row of stream) {
+            await processRow(row, bulkInsertData);
         }
 
-    }
-    catch (error) {
-        console.log("Error in populating the database", error);
-    }
-    finally {
+        // Insert any remaining records
+        if (bulkInsertData.length > 0) {
+            await bulkInsertAgricultureData([...bulkInsertData]);
+        }
+    } catch (error) {
+        console.log('Error in populating the database', error);
+    } finally {
         await sequelize.close();
     }
 }
-seedDatabase()
+
+// Start seeding the database
+seedDatabase();
